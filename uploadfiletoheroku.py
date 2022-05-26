23fbcdafb064745472e0a8fe8457da51368df21a
anugrah2002 <anugrahnigam16@gmail.com>
2022-05-26 13:42:47 +0530

    initial

---
+++ uploadfiletoheroku.py
@@ -0,0 +1,33 @@
+# from django.http import HttpResponse, JsonResponse
+import requests
+import cloudinary
+import cloudinary.uploader
+
+
+cloudinary.config(
+  cloud_name = 'detxlswii',
+  api_key = '474368939111198',
+  api_secret = 'gAmfegRLeJKZC66RWtNlW0Pp6tM'
+)
+
+# 'http://ytserver.eu-gb.cf.appdomain.cloud/entertain_news/savevideoofaajtk/'
+
+def uploadvideotoheroku(filepath,YTtitle):
+    try:
+        print(filepath)
+        filepath = str(filepath)
+        # filepath = filepath.replace(' ','\ ')
+        filepath="final.mp4"
+        print(filepath)
+        myurl = 'http://127.0.0.1:8000/entertain_news/savevideoofaajtk/'
+        upload_data = cloudinary.uploader.upload_large(filepath)
+        videoPublicId=str(upload_data['public_id'])
+        videoUrl=str(upload_data['secure_url'])
+        print(videoPublicId)
+        print(videoUrl)
+        title=YTtitle 
+        getdata = requests.post(myurl,data={'title':title,'videoPublicId':videoPublicId,'videoUrl':videoUrl})
+        print(getdata.text)  
+        # r=requests.post('http://lit-sierra-15246.herokuapp.com/videoupload/')
+    except Exception as e:
+        print(e)
---
+++ uploadtofirebase.py
@@ -0,0 +1,44 @@
+from firebase import Firebase
+from datetime import datetime
+import requests
+from firebase_admin import storage as admin_storage, credentials, firestore
+
+
+def uploadfiletofirebase(filepath,YTtitle):
+
+  current_time=str(datetime.now())
+  print(current_time)
+
+
+  firebaseConfig = {
+      'apiKey': "AIzaSyBexTA9lK-ruTMWVWEFaAzSKuIrNBjZ7vs",
+      'authDomain': "tiktokvideos-378aa.firebaseapp.com",
+      'storageBucket': "tiktokvideos-378aa.appspot.com",
+      "databaseURL": "https://tiktokvideos-378aa-default-rtdb.firebaseio.com/",
+      "serviceAccount": "./service_account.json"
+    }
+
+
+    # https://tiktokvideos-378aa-default-rtdb.firebaseio.com/
+
+
+
+  firebase = Firebase(firebaseConfig)
+
+  storage = firebase.storage()
+
+  # as admin
+  storage.child(filepath[:-5]+current_time+'.mp4').put(filepath)
+  url=storage.child(filepath[:-5]+current_time+'.mp4').get_url(1)
+  print(url)
+  nameofVideo=filepath[:-5]+current_time+'.mp4'
+
+# 'http://ytserver.eu-gb.cf.appdomain.cloud/entertain_news/savevideoofaajtk/'
+
+  myurl = 'http://127.0.0.1:8000/entertain_news/savevideoofaajtk/'
+
+  
+  titletopost = YTtitle
+
+  
+  postdatas = requests.post(myurl,data={'title':titletopost,'videoUrl':url,'name':nameofVideo})
