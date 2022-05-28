a5bbb1024900882c10953037b91d7c0796086396
Anugrah2002 <anugrahnigam16@gmail.com>
2022-05-28 17:31:41 +0000

    pushing vakyansh part

--- vakyansh_translation_hindi_audio.py
+++ vakyansh_translation_hindi_audio.py
@@ -22,7 +22,7 @@ os.system('wget https://storage.googleapis.com/vakyaansh-open-models/translit_mo
 os.system('mkdir hindi')
 os.chdir('hindi')
 os.system('wget https://storage.googleapis.com/vakyaansh-open-models/translit_models/hindi/hindi_transliteration.zip')
-os.system('unzip hindi_transliteration')
+os.system('unzip hindi_transliteration.zip')
 
 os.system ('wget https://storage.googleapis.com/vakyansh-open-models/tts/hindi/hi-IN/female_voice_0/glow.zip')
 os.system('unzip glow.zip')
@@ -33,13 +33,12 @@ os.system('unzip hifi.zip')
 os.system('rm glow.zip')
 os.system('rm hifi.zip')
 
-os.chdir('/workspace/EntertainmentNews_FromAjjtk/vakyansh-tts')
-os.chdir('vakyansh-tts')
+os.chdir('../../')
 
 os.system('pip install unidecode')
 os.system('pip3 uninstall numpy -y')
 os.system('rm -rf ~/.local/lib/python3.6/site-packages/numpy')
-os.system('pip install numpy==1.20.2')
+os.system('pip install numpy')
 
 from tts_infer.tts import TextToMel, MelToWav
 from tts_infer.transliterate import XlitEngine
@@ -49,49 +48,22 @@ import re
 from scipy.io.wavfile import write
 device = 'cpu'
 
-text_to_mel = TextToMel(glow_model_dir='/workspace/EntertainmentNews_FromAjjtk/vakyansh-tts/tts_infer/translit_models/hindi/glow_ckp', device=device)
-mel_to_wav = MelToWav(hifi_model_dir='/workspace/EntertainmentNews_FromAjjtk/vakyansh-tts/tts_infer/translit_models/hindi/hifi_ckp', device=device)
-text_to_mel = TextToMel(glow_model_dir='vakyansh-tts/tts_infer/translit_models/hindi/glow_ckp', device=device)
-mel_to_wav = MelToWav(hifi_model_dir='vakyansh-tts/tts_infer/translit_models/hindi/hifi_ckp', device=device)
+text_to_mel = TextToMel(glow_model_dir='translit_models/hindi/glow_ckp', device=device)
+mel_to_wav = MelToWav(hifi_model_dir='translit_models/hindi/hifi_ckp', device=device)
 
 def translit(text, lang):
-    print('translit line 55')
-    try:
-        print('translit line 55')
-        reg = re.compile(r'[a-zA-Z]')
-        print('translit line 55')
-        engine = XlitEngine(lang)
-        print('translit line 55')
-        words = [engine.translit_word(word, topk=1)[lang][0] if reg.match(word) else word for word in text.split()]
-        print('translit line 55')
-        updated_sent = ' '.join(words)
-        print('translit line 55')
-        return updated_sent
-    except Exception as e:
-        print(e)
-
+    reg = re.compile(r'[a-zA-Z]')
+    engine = XlitEngine(lang)
+    words = [engine.translit_word(word, topk=1)[lang][0] if reg.match(word) else word for word in text.split()]
+    updated_sent = ' '.join(words)
+    return updated_sent
     
 def run_tts(text, lang):
-    print('runtts line 72')
-    print(text)
-    try:
-        text = text.replace('।', '.') # only for hindi models
-        print('runtts line 68')
-        text_num_to_word = normalize_nums(text, lang) # converting numbers to words in lang
-        print('runtts line 78')
-        text_num_to_word_and_transliterated = translit(text_num_to_word, lang) # transliterating english words to lang
-        print('runtts line 80')
-        
-        mel = text_to_mel.generate_mel(text_num_to_word_and_transliterated, noise_scale=0.632, length_scale=0.80)
-        print('runtts line 83')
-        print(type(mel))
-        print('runtts line 85')
-        print(mel)
-        audio, sr = mel_to_wav.generate_wav(mel)
-        print('runtts line 88')
-        write(filename='temp.wav', rate=sr, data=audio) # for saving wav file, if needed
-        print('runtts line 87')
-        return (sr, audio)
-    except Exception as e:
-        print(e)
-
+    text = text.replace('।', '.') # only for hindi models
+    text_num_to_word = normalize_nums(text, lang) # converting numbers to words in lang
+    text_num_to_word_and_transliterated = translit(text_num_to_word, lang) # transliterating english words to lang
+    
+    mel = text_to_mel.generate_mel(text_num_to_word_and_transliterated, noise_scale=0.632, length_scale=0.80)
+    audio, sr = mel_to_wav.generate_wav(mel)
+    write(filename='temp.wav', rate=sr, data=audio) # for saving wav file, if needed
+    return (sr, audio)
