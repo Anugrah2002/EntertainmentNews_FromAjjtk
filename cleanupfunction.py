cc5617d489f7f717a85baa5c0309eb253e2edb05
anugrah2002 <anugrahnigam16@gmail.com>
2022-06-04 15:01:09 +0530

    cleanupfunction for aajtk

---
+++ cleanupfunction.py
@@ -0,0 +1,40 @@
+# from firebase import Firebase
+from firebase import Firebase
+from datetime import datetime
+import requests, json
+import re
+import firebase_admin
+from firebase_admin import storage as admin_storage, credentials, firestore
+# from firebase_admin import bucket
+
+
+firebaseConfig = {
+    'apiKey': "AIzaSyBexTA9lK-ruTMWVWEFaAzSKuIrNBjZ7vs",
+    'authDomain': "tiktokvideos-378aa.firebaseapp.com",
+    'storageBucket': "tiktokvideos-378aa.appspot.com",
+    "databaseURL": "https://tiktokvideos-378aa-default-rtdb.firebaseio.com/",
+    "serviceAccount": "./service_account.json",
+}
+
+firebase = Firebase(firebaseConfig)
+storage=firebase.storage()
+
+
+def cleanupFunction():
+    data=requests.get('http://ytserver.eu-gb.cf.appdomain.cloud/entertain_news/clean/')
+    data=data.json()
+    print(data)
+
+    for i in data:
+        data=i['videoPublicId']
+        # NextDay_Date = datetime.datetime.today() - datetime.timedelta(days=2)
+        # formatted= NextDay_Date.strftime("%y-%m-%d")
+        print(data)
+        storage.delete(data)
+        return 'ok'
+
+cleanupFunction()
+
+
+
+    
